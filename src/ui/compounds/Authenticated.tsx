9700fc27cd34334dabe0d111e4d9d9849334be23
Alan R. Soares <alanrsoars@gmail.com>
2021-03-06 12:42:31 +1300

    chore: split GithubCard component

--- src/ui/compounds/Authenticated.tsx
+++ src/ui/compounds/Authenticated.tsx
@@ -1,6 +1,8 @@
 import { sdk } from "lib/github-client";
 import { useQuery } from "react-query";
 
+import GithubCard from "ui/components/GithubCard";
+
 const Authenticated: React.FC = () => {
   const { data, isLoading, error } = useQuery("viewer", () => sdk.viewer());
 
@@ -16,6 +18,7 @@ const Authenticated: React.FC = () => {
     if (error) {
       console.log("Something went wrong!", error);
     }
+
     return (
       <div className="max-w-xs border border-black rounded-lg p-4 bg-gray-800 text-white">
         Something went wrong!
@@ -23,26 +26,7 @@ const Authenticated: React.FC = () => {
     );
   }
 
-  return (
-    <div className="max-w-xs border border-black rounded-lg p-4 bg-gray-800 text-white">
-      <figure className="p-2">
-        <img
-          src={data.viewer.avatarUrl}
-          alt="Github avatar"
-          className="h-36 w-36 rounded-full mx-auto"
-        />
-      </figure>
-      <h2 className="text-4xl py-2">{data.viewer.name}</h2>
-      <div className="text-sm bg-gray-50 rounded-md text-gray-700 p-2">
-        {data.viewer.bio}
-      </div>
-      <div className="py-2">@{data.viewer.login}</div>
-      <div className="p-4">
-        <div>{data.viewer.followers.totalCount} followers</div>
-        <div>{data.viewer.repositories.totalCount} repositories</div>
-      </div>
-    </div>
-  );
+  return <GithubCard viewer={data.viewer} />;
 };
 
 export default Authenticated;
